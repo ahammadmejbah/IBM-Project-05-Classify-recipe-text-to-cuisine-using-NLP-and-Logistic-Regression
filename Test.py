6afac383ee081c60eef3460106a87ee76a58dd48
ahammadmejbah <ahammadmejbah@gmail.com>
2023-09-30 04:37:45 +0600

    https://github.com/ahammadmejbah

--- Test.py
+++ Test.py
@@ -2,12 +2,12 @@ import os
 from random import randint
 
 for i in range(1, 365):
-    for j in range(0, randint(1, 1000)):
+    for j in range(0, randint(1, 10)):
         d = str(i) + "days ago"
         with open('file.txt', 'a') as file:
             file.write(d)
 
         os.system('git add .')
-        os.system('git commit --date="' + d + '" -m "commit"')
+    os.system('git commit --date="' + d + '" -m "commit"')
 
-    os.system('git push -u origin main')
+os.system('git push -u origin main')
